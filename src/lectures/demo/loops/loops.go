d819c902567d93b99126b8f914227341fc549df7
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-10-27 11:15:08 +0530

    Loops and switches

--- src/lectures/demo/loops/loops.go
+++ src/lectures/demo/loops/loops.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 
+	sum := 0
+	fmt.Println("Sum is", sum)
+
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Println("Sum is", sum, " and i is", i)
+	}
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("Decrement: Sum is now", sum)
+	}
 }
--- src/lectures/exercise/switch/switch.go
+++ src/lectures/exercise/switch/switch.go
@@ -14,4 +14,23 @@ package main
 import "fmt"
 
 func main() {
+
+	switch age := 19; {
+	case age == 0:
+		fmt.Println("newborn")
+		//  - "newborn" when age is 0
+	case 1 >= age && age <= 3:
+		fmt.Println("toddler")
+		//  - "toddler" when age is 1, 2, or 3
+	case age >= 4 && age <= 12:
+		fmt.Println("child")
+		//  - "child" when age is 4 through 12
+	case age >= 13 && age <= 17:
+		fmt.Println("teenager")
+		//  - "teenager" when age is 13 through 17
+	default:
+		fmt.Println("adult")
+		//  - "adult" when age is 18+
+	}
+
 }
