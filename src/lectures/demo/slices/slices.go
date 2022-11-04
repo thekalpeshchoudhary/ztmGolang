e59edb001f5a307e49bb9fef67fbf09a97b991e2
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-11-04 17:44:22 +0530

    SLices and arrays

--- src/lectures/demo/slices/slices.go
+++ src/lectures/demo/slices/slices.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+func printSlices(title string, slice []string) {
+	fmt.Println()
+	fmt.Println("---", title, "---")
+	for i := 0; i < len(slice); i++ {
+		element := slice[i]
+		fmt.Println(element)
+	}
+}
 func main() {
+	route := []string{"Grocery", "Department Store", "Salon"}
+	printSlices("Route 1", route)
 
+	route = append(route, "Home")
+	printSlices("Route 2", route)
+
+	fmt.Println()
+	fmt.Println(route[0], "visited")
+	fmt.Println(route[1], "visited")
+
+	route = route[2:]
+	printSlices("Route 3", route)
 }
--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,45 @@ package main
 
 import "fmt"
 
+//  - Products must include the price and the name
+type Product struct {
+	name  string
+	price int
+}
+
+//* Print to the terminal:
+func printShoppingList(shoppingList [4]Product) {
+	var items, total int
+	for i := 0; i < len(shoppingList); i++ {
+		eachItem := shoppingList[i]
+		total += eachItem.price
+		if eachItem.name != "" {
+			items += 1
+		}
+	}
+	//  - The last item on the list
+	fmt.Println("Last Item :", shoppingList[items-1])
+	//  - The total number of items
+	fmt.Println("Total Price", total)
+	//  - The total cost of the items
+	fmt.Println("Total number of Items:", items)
+}
+
 func main() {
+	//* Using an array, create a shopping list with enough room
+	//  for 4 products
+
+	//* Insert 3 products into the array
+	shoppingList := [4]Product{
+		{name: "Phone", price: 50000},
+		{name: "Charger", price: 3200},
+		{name: "Headphones", price: 8850},
+	}
+	printShoppingList(shoppingList)
+
+	//* Add a fourth product to the list and print out the
+	//  information again
+	shoppingList[3] = Product{name: "Watch", price: 25000}
+	printShoppingList(shoppingList)
 
 }
