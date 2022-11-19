37aa14e80cd15458855fc2e796706a6253df9959
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-11-19 10:57:45 +0530

    intefaces

--- src/lectures/demo/interfaces/interfaces.go
+++ src/lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+type Preparer interface {
+	PrepareDish()
+}
+
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("Cook Chicken")
+}
+func (s Salad) PrepareDish() {
+	fmt.Println("Chop")
+	fmt.Println("Add Dressing")
+}
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare Dishes :")
+	for i := 0; i < len(dishes); i++ {
+		dish := dishes[i]
+		fmt.Printf("--Dish: %v \n", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
+
 func main() {
+	dishes := []Preparer{
+		Chicken("Chicken Wings"), Salad("Iceberg Salad"),
+	}
+	prepareDishes(dishes)
 
 }
--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -19,8 +19,73 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// * The shop has lifts for multiple vehicle sizes/types:
+//   - Motorcycles: small lifts
+//   - Cars: standard lifts
+//   - Trucks: large lifts
+const (
+	SmallLift = iota
+	StandardLift
+	LargeLift
+)
+
+type Lift int
+
+type LiftPicker interface {
+	PickLift() Lift
+}
+
+type Car string
+type Truck string
+type Bike string
+
+// * Vehicles have a model name in addition to the vehicle type:
+//   - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
+func (b Bike) String() string {
+	return fmt.Sprintf("Bike : %v", string(b))
+}
+func (c Car) String() string {
+	return fmt.Sprintf("Car : %v", string(c))
+}
+func (t Truck) String() string {
+	return fmt.Sprintf("Truck : %v", string(t))
+}
+
+func (b Bike) PickLift() Lift {
+	return SmallLift
+}
+func (c Car) PickLift() Lift {
+	return StandardLift
+}
+func (t Truck) PickLift() Lift {
+	return LargeLift
+}
+
+//   - Write a single function to handle all of the vehicles
+//     that the shop works on.
+func sendToLift(p LiftPicker) {
+	switch p.PickLift() {
+	case SmallLift:
+		fmt.Printf("Send %v to Small lift\n", p)
+	case StandardLift:
+		fmt.Printf("Send %v to Standard lift\n", p)
+	case LargeLift:
+		fmt.Printf("Send %v to Large Lift lift\n", p)
+	}
+}
 
 func main() {
+	//* Direct at least 1 of each vehicle type to the correct
+	//  lift, and print out the vehicle information.
+	car := Car("Nexon")
+	truck := Truck("Ashok Leyland")
+	bike := Bike("KTM")
 
+	sendToLift(bike)
+	sendToLift(car)
+	sendToLift(truck)
 }
