0bbd3a17cd85b28e79ad407eb9d2b6d3add421ce
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-11-25 12:30:06 +0530

    embedding, readers

--- src/lectures/exercise/embedding/embedding.go
+++ src/lectures/exercise/embedding/embedding.go
@@ -12,9 +12,7 @@
 
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Bytes int
 type Celcius float32
@@ -31,8 +29,51 @@ type MemoryUsage struct {
 	amount []Bytes
 }
 
+type DashboardComponents struct {
+	BandwidthUsage
+	CpuTemp
+	MemoryUsage
+}
+
+type BandwidthUsageAverage interface {
+	CalAverageBandwidth() Bytes
+}
+type MemoryUsageAverage interface {
+	CalAverageMemory() Bytes
+}
+type CpuUsageAverage interface {
+	CalAverageCpu() Celcius
+}
+
+func (b *BandwidthUsage) CalAverageBandwidth() Bytes {
+	var total Bytes = 0
+	for _, eachByte := range b.amount {
+		total += eachByte
+	}
+	return total / 5
+}
+func (m *MemoryUsage) CalAverageMemory() Bytes {
+	var total Bytes = 0
+	for _, eachByte := range m.amount {
+		total += eachByte
+	}
+	return total / 5
+}
+func (t *CpuTemp) CalAverageCpu() Celcius {
+	var total Celcius = 0
+	for _, eachCelc := range t.temp {
+		total += eachCelc
+	}
+	return total / 5
+}
+
 func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+
+	dashboardComponents := DashboardComponents{bandwidth, temp, memory}
+	fmt.Printf("Average Bandwidth Usage is %v\n", dashboardComponents.CalAverageBandwidth())
+	fmt.Printf("Average Memory Usage is %v\n", dashboardComponents.CalAverageMemory())
+	fmt.Printf("Average Cpu Temp is %v\n", dashboardComponents.CalAverageCpu())
 }
--- src/lectures/exercise/reader/reader.go
+++ src/lectures/exercise/reader/reader.go
@@ -18,8 +18,72 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	CommandBye   = "bye"
+	CommandHello = "hello"
+)
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	nonBlankLine, commands := 0, 0
+	for scanner.Scan() {
+		theCommand := strings.ToLower(strings.TrimSpace(scanner.Text()))
+		if theCommand == "q" {
+			break
+		} else {
+			switch theCommand {
+			case CommandBye:
+				commands += 1
+				fmt.Println("Bye Bye!")
+			case CommandHello:
+				commands += 1
+				fmt.Println("Hello There!")
+			}
+			if theCommand != "" {
+				nonBlankLine += 1
+			}
+		}
+	}
+
+	// Reader method
+	// r := bufio.NewReader(os.Stdin)
+	// for {
+	// 	input, inputErr := r.ReadString('\n')
+	// 	n := strings.TrimSpace(input)
+	// 	//* When the user enters either "hello" or "bye", the program
+	// 	//  should respond with a message after pressing the enter key.
+	// 	if n == "hello" {
+	// 		commands += 1
+	// 		nonBlankLine += 1
+	// 		fmt.Println("Hello there")
+	// 	}
+	// 	if n == "bye" {
+	// 		commands += 1
+	// 		nonBlankLine += 1
+	// 		fmt.Println("Bye Bye!")
+	// 	}
+	// 	//* Whenever the user types a "Q" or "q", the program should exit.
+	// 	//* Upon program exit, some usage statistics should be printed
+	// 	if n == "Q" || n == "q" {
+	// 		fmt.Println("Goodbye!")
+	// 		break
+	// 	}
+	// 	if n == "" {
+	// 		continue
+	// 	}
+	// 	if inputErr != nil {
+	// 		fmt.Println("Inout error:", inputErr)
+	// 	}
+	// }
+	//  - The number of non-blank lines entered
+	//  - The number of commands entered
+	fmt.Printf("---Stats \n Non-blank lines: %v \n commands entered : %v", nonBlankLine, commands)
 
 }
