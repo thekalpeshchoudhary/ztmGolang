aec0f9177025a103533fd113dcb285a2b59a083a
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-12-02 14:58:07 +0530

    channels

--- src/lectures/demo/channels/channels.go
+++ src/lectures/demo/channels/channels.go
@@ -1,8 +1,65 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+type ControlMsg int
+
+const (
+	DoExit = iota
+	ExitOk
+)
+
+type Job struct {
+	data int
+}
+
+type Result struct {
+	result int
+	job    Job
+}
+
+func doubler(jobs <-chan Job, results chan<- Result, control chan ControlMsg) {
+	for {
+		select {
+		case msg := <-control:
+			switch msg {
+			case DoExit:
+				fmt.Println("Exit goroutine")
+				control <- ExitOk
+				return
+			default:
+				panic("Unhandled control msg")
+			}
+		case job := <-jobs:
+			results <- Result{result: job.data * 2, job: job}
+		}
+	}
+}
 
 func main() {
+	jobs := make(chan Job, 50)
+	results := make(chan Result, 50)
+	control := make(chan ControlMsg)
 
+	go doubler(jobs, results, control)
+
+	for i := 0; i < 30; i++ {
+		jobs <- Job{i}
+	}
+
+	for {
+		select {
+		case result := <-results:
+			fmt.Println(result)
+		case <-time.After(500 * time.Millisecond):
+			fmt.Println("Timed out")
+			control <- DoExit
+			<-control
+			fmt.Println("Program exit")
+			return
+		}
+	}
 }
--- src/lectures/exercise/channels/channels.go
+++ src/lectures/exercise/channels/channels.go
@@ -14,9 +14,11 @@
 
 package main
 
-import "fmt"
-import "time"
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Job int
 
@@ -35,7 +37,29 @@ func makeJobs() []Job {
 	return jobs
 }
 
+func runCalc(j Job, results chan int) {
+	results <- longCalculation(j)
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	jobs := makeJobs()
+
+	results := make(chan int, 10)
+
+	for i := 0; i < len(jobs); i++ {
+		go runCalc(jobs[i], results)
+	}
+
+	resultCount := 0
+	sum := 0
+	for {
+		result := <-results
+		sum += result
+		resultCount += 1
+		if resultCount == len(jobs) {
+			break
+		}
+	}
+	fmt.Println("Result is :", sum)
 }
